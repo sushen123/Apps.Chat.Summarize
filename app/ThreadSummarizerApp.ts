ab35567c0b2627514bfc391bd00cc5c71b74a29e
Sushen Oli <sushensame@gmail.com>
2025-03-29 13:34:59 +0545

    Add action button

--- app/ThreadSummarizerApp.ts
+++ app/ThreadSummarizerApp.ts
@@ -7,6 +7,9 @@ import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { SummarizeCommand } from './commands/SummarizeCommand';
 import { settings } from './settings/settings';
+import { ActionButton } from './enum/ActionButton';
+import {  MessageActionContext, UIActionButtonContext } from '@rocket.chat/apps-engine/definition/ui';
+
 
 export class ThreadSummarizerApp extends App {
 	constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
@@ -14,6 +17,30 @@ export class ThreadSummarizerApp extends App {
 	}
 
 	public async extendConfiguration(configuration: IConfigurationExtend) {
+
+        configuration.ui.registerButton({
+            actionId: ActionButton.OPEN_SUMMARIZE_MODAL_ACTION,
+            labelI18n: ActionButton.OPEN_SUMMARIZE_MODAL_LABEL,
+            context: UIActionButtonContext.MESSAGE_ACTION,
+
+            when: {
+                messageActionContext: [
+                    MessageActionContext.MESSAGE
+                ]
+            }
+        })
+
+        configuration.ui.registerButton({
+            actionId: ActionButton.OPEN_SUMMARIZE_MODAL_ACTION,
+            labelI18n: ActionButton.OPEN_SUMMARIZE_MODAL_LABEL,
+            context: UIActionButtonContext.MESSAGE_BOX_ACTION,
+
+            when: {
+                messageActionContext: [
+                    MessageActionContext.MESSAGE
+                ]
+            }
+        })
 		await Promise.all([
 			...settings.map((setting) =>
 				configuration.settings.provideSetting(setting)
---
+++ app/enum/ActionButton.ts
@@ -0,0 +1,4 @@
+export enum ActionButton {
+    OPEN_SUMMARIZE_MODAL_ACTION = 'open-summarize-model-action',
+    OPEN_SUMMARIZE_MODAL_LABEL = 'Summarize_the_thread'
+}
